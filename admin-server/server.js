4b47ea99d37d7ddf95170d46c5b42e2a6c5c36ee
Yulia <yuliiasaiylovskay@gmail.com>
2025-02-27 17:59:07 +0300

    ред.2

--- admin-server/server.js
+++ admin-server/server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const app = express();
 const PORT = 8080;
 
+// Загрузка Swagger спецификации
+const swaggerDocument = YAML.load(path.join(__dirname, 'api-spec.yaml'));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('admin-server'));
+
+// Добавляем Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Добавляем явный маршрут для корневого пути
 app.get('/', (req, res) => {
